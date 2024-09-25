5079f1a703adc68cbc0c6b4cd25a49f5bcd54ac5
MuyleangIng <muyleanging@gmail.com>
2024-09-26 00:59:15 +0700

    dfd

--- vars/checkProjectType.groovy
+++ vars/checkProjectType.groovy
@@ -1,24 +1,48 @@
-def call() {
+// def call() {
+//     def type = [:]
+    
+//     if (fileExists('package.json')) {
+//         type.frontend = 'react'
+//         if (fileExists('next.config.js')) {
+//             type.frontend = 'nextjs'
+//         }
+//     }
+    
+//     if (fileExists('pom.xml')) {
+//         type.backend = 'maven'
+//     } else if (fileExists('build.gradle') || fileExists('build.gradle.kts')) {
+//         type.backend = 'gradle'
+//     } else if (fileExists('main.py') && fileExists('requirements.txt')) {
+//         type.backend = 'fastapi'
+//     }
+    
+//     if (fileExists('flyway.conf') || fileExists('liquibase.properties')) {
+//         type.database = 'postgres'
+//     }
+    
+//     return type
+// }
+def checkProjectType() {
     def type = [:]
     
     if (fileExists('package.json')) {
-        type.frontend = 'react'
+        type['frontend'] = 'react'
         if (fileExists('next.config.js')) {
-            type.frontend = 'nextjs'
+            type['frontend'] = 'nextjs'
         }
     }
     
     if (fileExists('pom.xml')) {
-        type.backend = 'maven'
+        type['backend'] = 'maven'
     } else if (fileExists('build.gradle') || fileExists('build.gradle.kts')) {
-        type.backend = 'gradle'
+        type['backend'] = 'gradle'
     } else if (fileExists('main.py') && fileExists('requirements.txt')) {
-        type.backend = 'fastapi'
+        type['backend'] = 'fastapi'
     }
     
     if (fileExists('flyway.conf') || fileExists('liquibase.properties')) {
-        type.database = 'postgres'
+        type['database'] = 'postgres'
     }
     
-    return type
+    return type  // Return a map with keys like 'frontend', 'backend', 'database'
 }
