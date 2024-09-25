19b8f02456d69999924055b81368274a125f676b
MuyleangIng <muyleanging@gmail.com>
2024-09-26 01:16:24 +0700

    update

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -3,13 +3,18 @@ import org.istad.Backend
 import org.istad.Database
 
 def call(Map projectType) {
-    if (projectType['frontend']) {
-        Frontend.runTests()
-    }
-    if (projectType.backend) {
-        Backend.runTests(projectType.backend)
-    }
-    if (projectType.database) {
-        Database.runTests()
+    try {
+        if (projectType['frontend']) {
+            Frontend.runTests()
+        }
+        if (projectType.backend) {
+            Backend.runTests(projectType.backend)
+        }
+        if (projectType.database) {
+            Database.runTests()
+        }
+    } catch (Exception e) {
+        echo "Error running tests: ${e.message}"
+        currentBuild.result = 'FAILURE'
     }
 }
