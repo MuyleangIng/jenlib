ecf067775943c907d8e51836390138a0cc20176a
MuyleangIng <muyleanging@gmail.com>
2024-09-26 07:31:24 +0700

    update env

--- vars/writeDockerfile.groovy
+++ vars/writeDockerfile.groovy
@@ -1,5 +1,15 @@
-def call(String projectType) {
-    def dockerfileContent = libraryResource("${projectType}-dockerfile")
+// 
+def call(Map projectType) {
+    def dockerfileType
+    if (projectType.frontend == 'nextjs') {
+        dockerfileType = 'next'
+    } else if (projectType.frontend == 'react') {
+        dockerfileType = 'react'
+    } else {
+        error "Unsupported frontend type: ${projectType.frontend}"
+    }
+
+    def dockerfileContent = libraryResource("${dockerfileType}-dockerfile")
     writeFile file: 'Dockerfile', text: dockerfileContent
-    println "Dockerfile for ${projectType} has been written to the project root."
+    println "Dockerfile for ${dockerfileType} has been written to the project root."
 }
