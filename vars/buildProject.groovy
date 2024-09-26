61cd8ffa86400cc24b6cfc95a4b1a42998d6b4f7
MuyleangIng <muyleanging@gmail.com>
2024-09-26 07:26:35 +0700

    update env

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -4,12 +4,12 @@ import org.istad.Database
 
 def call(Map projectType) {
     if (projectType.frontend) {
-        Frontend.build()
+        Frontend.build(this)
     }
     if (projectType.backend) {
-        Backend.build(projectType.backend)
+        Backend.build(projectType.backend, this)
     }
     if (projectType.database) {
-        Database.migrate()
+        Database.migrate(this)
     }
 }
